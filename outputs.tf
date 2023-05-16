05e9aeb0e170ca3e1d4eee3093f1257a3a09776f
Federico Sismondi <federico.sismondi@camptocamp.com>
2023-05-16 11:53:35 +0200

    feat(output): export workload identity values

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,14 @@ output "helm_values" {
   description = "Helm values applied to the module's chart"
   value       = data.utils_deep_merge_yaml.values.output
 }
+
+output "azure_identities" {
+  description = "Azure User Assigned Identities created"
+  value = [for v in var.workload_identities :
+    tomap({
+      name      = v.name
+      namespace = v.namespace
+      clientID  = azurerm_user_assigned_identity.this[format("%s.%s", v.namespace, v.name)].client_id
+    })
+  ]
+}
